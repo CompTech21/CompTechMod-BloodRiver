e1cc0584c00fbaa56398be34d930847cb3bd0242
CompTech21 <danielvood12@gmail.com>
2025-10-05 10:55:18 +0300

    .

--- Common/Systems/DontDoThisEffects.cs
+++ Common/Systems/DontDoThisEffects.cs
@@ -1,51 +1,185 @@
+using System.Collections.Generic;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
-using CompTechMod.Content.Items;
+using Microsoft.Xna.Framework;
+using Terraria.DataStructures;
+using System;
 
 namespace CompTechMod.Common.Systems
 {
     public class DontDoThisEffects : ModSystem
     {
-        // Правильный способ замедлить ночь
-        public override void ModifyTimeRate(ref double timeRate, ref double tileUpdateRate, ref double eventUpdateRate)
-        {
-            if (!CompWorld.DontDoThisMode)
-                return;
+        private static readonly Dictionary<int, int> guardianSpawnCooldown = new();
+        private static bool guardiansSpawnedOnJoin = false;
 
-            if (!Main.dayTime)
-            {
-                timeRate *= 0.5; // ночь ×2
-            }
-        }
+        public override void OnWorldLoad() => guardiansSpawnedOnJoin = false;
 
         public override void PostUpdateWorld()
         {
-            if (!CompWorld.DontDoThisMode)
-                return;
+            if (!CompWorld.DontDoThisMode) return;
 
-            // 2. Всегда Blood Moon ночью
+            // Ночь чуть быстрее
             if (!Main.dayTime)
+            {
                 Main.bloodMoon = true;
+                Main.time += 0.7;
+            }
+
+            // Гарантированный спавн Dungeon Guardian при входе
+            if (!guardiansSpawnedOnJoin)
+            {
+                guardiansSpawnedOnJoin = true;
+                foreach (Player player in Main.player)
+                    if (player.active)
+                        SpawnGuardians(player, 6);
+            }
         }
 
         public override void PostUpdatePlayers()
         {
-            if (!CompWorld.DontDoThisMode)
-                return;
+            if (!CompWorld.DontDoThisMode) return;
 
             foreach (Player player in Main.player)
             {
                 if (!player.active) continue;
 
-                // 3. Все характеристики уменьшаем в 2 раза (кроме HP и маны)
-                player.statDefense *= 0.5f; // работает с DefenseStat
+                player.statDefense *= 0.5f;
                 player.GetDamage(DamageClass.Generic) *= 0.5f;
                 player.moveSpeed *= 0.5f;
-                player.pickSpeed *= 2f; // копает хуже
-
-                // 4. -1 слот аксессуаров
+                player.pickSpeed *= 0.2f;
                 if (player.extraAccessorySlots > 0)
-                    player.extraAccessorySlots = player.extraAccessorySlots - 1;
+                    player.extraAccessorySlots--;
+
+                if (!guardianSpawnCooldown.ContainsKey(player.whoAmI))
+                    guardianSpawnCooldown[player.whoAmI] = 0;
+
+                if (guardianSpawnCooldown[player.whoAmI] > 0)
+                {
+                    guardianSpawnCooldown[player.whoAmI]--;
+                    continue;
+                }
+
+                if (Main.rand.NextFloat() < 0.05f)
+                {
+                    SpawnGuardians(player, 8);
+                    guardianSpawnCooldown[player.whoAmI] = 720000;
+                }
+            }
+        }
+
+        private void SpawnGuardians(Player player, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Vector2 spawnPos = player.Center + new Vector2(Main.rand.Next(-400, 400), Main.rand.Next(-400, 400));
+                int npcIndex = NPC.NewNPC(null, (int)spawnPos.X, (int)spawnPos.Y, NPCID.DungeonGuardian);
+                if (npcIndex >= 0)
+                {
+                    Main.npc[npcIndex].target = player.whoAmI;
+                    Main.npc[npcIndex].friendly = false;
+                }
+            }
+        }
+    }
+
+    public class DontDoThisItemGlobal : GlobalItem
+    {
+        public override bool CanUseItem(Item item, Player player)
+        {
+            if (!CompWorld.DontDoThisMode) return true;
+
+            if (item.DamageType != DamageClass.Default || item.pick > 0 || item.axe > 0 || item.hammer > 0)
+                if (Main.rand.NextFloat() < 0.02f)
+                {
+                    item.TurnToAir();
+                    return false;
+                }
+
+            return true;
+        }
+    }
+
+    public class DontDoThisGlobalNPC : GlobalNPC
+    {
+        public override bool AppliesToEntity(NPC npc, bool lateInstantiation) => npc.townNPC;
+
+        public override void AI(NPC npc)
+        {
+            if (!CompWorld.DontDoThisMode) return;
+            npc.friendly = false;
+            npc.target = npc.FindClosestPlayer();
+        }
+    }
+
+    public class DontDoThisGlobalPlayer : ModPlayer
+    {
+        private readonly int[] tombstoneProjectiles = new int[]
+        {
+            ProjectileID.Headstone,
+            ProjectileID.CrossGraveMarker,
+            ProjectileID.GraveMarker,
+            ProjectileID.Obelisk
+        };
+
+        private bool tombstonesSpawned = false; // новый флаг
+
+        public override void PreUpdate()
+        {
+            if (!CompWorld.DontDoThisMode) return;
+
+            if (Player.dead && !tombstonesSpawned)
+            {
+                SpawnTombstones();
+                tombstonesSpawned = true; // помечаем, что уже спавнили
+            }
+
+            // Сбрасываем флаг, когда игрок возрождается
+            if (!Player.dead && tombstonesSpawned)
+            {
+                tombstonesSpawned = false;
+            }
+        }
+
+        public void SpawnTombstones()
+        {
+            float radius = 128f; // максимальное смещение от игрока
+            for (int i = 0; i < 5; i++)
+            {
+                // равномерное распределение по кругу
+                float angle = MathHelper.TwoPi * i / 5f + Main.rand.NextFloat(-0.2f, 0.2f);
+                Vector2 offset = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle)) * (Main.rand.NextFloat(64f, radius));
+                Vector2 pos = Player.Center + offset;
+
+                int projType = tombstoneProjectiles[Main.rand.Next(tombstoneProjectiles.Length)];
+
+                Projectile.NewProjectile(
+                    Player.GetSource_Death(),
+                    pos,
+                    Vector2.Zero,
+                    projType,
+                    0,
+                    0f,
+                    Player.whoAmI
+                );
+            }
+        }
+    }
+
+    public class DontDoThisGlobalTile : GlobalTile
+    {
+        public override void KillTile(int i, int j, int type, ref bool fail, ref bool effectOnly, ref bool noItem)
+        {
+            if (!CompWorld.DontDoThisMode) return;
+
+            if (type == TileID.Tombstones)
+            {
+                Player player = Main.LocalPlayer;
+                if (player.active && !player.dead)
+                {
+                    player.KillMe(PlayerDeathReason.ByCustomReason(""), 9999.0, 0, false);
+                    player.GetModPlayer<DontDoThisGlobalPlayer>().SpawnTombstones();
+                }
             }
         }
     }
--- Content/Items/DontDoThis.cs
+++ Content/Items/DontDoThis.cs
@@ -1,104 +1,30 @@
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-using CompTechMod.Content.Items;
-using Microsoft.Xna.Framework;
+using CompTechMod.Common.Systems;
 
-namespace CompTechMod.Common.Systems
+namespace CompTechMod.Content.Items
 {
-    public class DontDoThisEffects : ModSystem
+    public class DontDoThis : ModItem
     {
-        public override void ModifyTimeRate(ref double timeRate, ref double tileUpdateRate, ref double eventUpdateRate)
+        public override void SetDefaults()
         {
-            if (!CompWorld.DontDoThisMode)
-                return;
-
-            if (!Main.dayTime)
-            {
-                timeRate *= 0.5; // ночь ×2 медленнее
-            }
+            Item.width = 32;
+            Item.height = 32;
+            Item.useStyle = ItemUseStyleID.HoldUp;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            Item.consumable = true;
+            Item.maxStack = 1;
+            Item.rare = ItemRarityID.Red;
         }
 
-        public override void PostUpdateWorld()
+        public override bool? UseItem(Player player)
         {
-            if (!CompWorld.DontDoThisMode)
-                return;
-
-            // Всегда Blood Moon ночью
-            if (!Main.dayTime)
-                Main.bloodMoon = true;
-        }
-
-        public override void PostUpdatePlayers()
-        {
-            if (!CompWorld.DontDoThisMode)
-                return;
-
-            foreach (Player player in Main.player)
-            {
-                if (!player.active) continue;
-
-                // Все характеристики уменьшены (кроме HP и маны)
-                player.statDefense = (int)(player.statDefense * 0.5f);
-                player.GetDamage(DamageClass.Generic) *= 0.5f;
-                player.moveSpeed *= 0.5f;
-                player.pickSpeed *= 4f;
-
-                // -1 слот аксессуаров
-                if (player.extraAccessorySlots > 0)
-                    player.extraAccessorySlots--;
-
-                // 5% шанс спавна 8 Dungeon Guardian
-                if (Main.rand.NextFloat() < 0.05f)
-                {
-                    for (int i = 0; i < 8; i++)
-                    {
-                        Vector2 spawnPos = player.Center + new Vector2(Main.rand.Next(-400, 400), Main.rand.Next(-400, 400));
-                        int npcIndex = NPC.NewNPC(null, (int)spawnPos.X, (int)spawnPos.Y, NPCID.DungeonGuardian);
-                        if (npcIndex >= 0 && Main.npc[npcIndex] != null)
-                        {
-                            Main.npc[npcIndex].target = player.whoAmI;
-                            Main.npc[npcIndex].friendly = false;
-                            Main.npc[npcIndex].npcSlots = 10f;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    // Удаление оружия/инструмента
-    public class DontDoThisItemGlobal : GlobalItem
-    {
-        public override bool CanUseItem(Item item, Player player)
-        {
-            if (!CompWorld.DontDoThisMode)
-                return true;
-
-            if (item.DamageType != DamageClass.Default || item.pick > 0 || item.axe > 0 || item.hammer > 0)
-            {
-                if (Main.rand.NextFloat() < 0.02f)
-                {
-                    item.TurnToAir();
-                    return false;
-                }
-            }
+            CompWorld.DontDoThisMode = true;
+            if (Main.netMode != NetmodeID.Server)
+                Main.NewText("DontDoThis Mode Activated!", 255, 50, 50);
             return true;
-        }
-    }
-
-    // Замена крафта
-    public class DontDoThisRecipe : GlobalItem
-    {
-        public override void OnCraft(Item item, Recipe recipe)
-        {
-            if (!CompWorld.DontDoThisMode)
-                return;
-
-            if (Main.rand.NextFloat() < 0.15f)
-            {
-                item.SetDefaults(ItemID.Poo);
-            }
         }
     }
 }
